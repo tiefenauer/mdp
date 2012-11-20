54c4608efbde65d8b806fdccdadd46eb1d933774
Daniel Tiefenauer <git@tiefenauer.info>
2012-11-20 11:17:58 +0100

    Maven Setup

---
+++ src/main/java/info/tiefenauer/mdp/App.java
@@ -0,0 +1,13 @@
+package info.tiefenauer.mdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
